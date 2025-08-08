9ce50ece8787b48d7ca1589d9349c7011582ca2c
koheiiiii <kou.0318.kou.0318@gmail.com>
2025-08-08 09:14:29 +0900

    IDの生成ロジック作成とdynamodbへの接続の修正

--- zuora-event-handler/ZuoraEventHandler/src/main/java/com/fujifilm/fb/spf/subscription/zuora/complement/SubscriptionIdGenerator.java
+++ zuora-event-handler/ZuoraEventHandler/src/main/java/com/fujifilm/fb/spf/subscription/zuora/complement/SubscriptionIdGenerator.java
@@ -2,8 +2,11 @@ package com.fujifilm.fb.spf.subscription.zuora.complement;
 
 import java.security.SecureRandom;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
 /**
- * サブスクリプションIDを生成するユーティリティクラスです。
+ * サブスクリプションIDを生成するクラスです。
  * <p>
  * 12桁のランダムな数字を4桁ごとにハイフンで区切った
  * 「nnnn-nnnn-nnnn」形式のIDを生成します。
@@ -11,14 +14,18 @@ import java.security.SecureRandom;
  * <pre>
  * 例: 1234-5678-9012
  * </pre>
- * インスタンス化はできません。
+ * このクラスはスレッドセーフであり、シングルトンとして使用されます。
  */
+@Singleton
 class SubscriptionIdGenerator {
     private static final int DIGIT_LENGTH = 12;
     private static final int BLOCK_SIZE = 4;
+    
+    private final SecureRandom random;
 
-    private SubscriptionIdGenerator() {
-        // プライベートコンストラクタでインスタンス化を防ぐ
+    @Inject
+    SubscriptionIdGenerator() {
+        this.random = new SecureRandom();
     }
 
     /**
@@ -26,8 +33,7 @@ class SubscriptionIdGenerator {
      *
      * @return nnnn-nnnn-nnnn 形式のランダムID
      */
-    public static String generate(){
-        SecureRandom random = new SecureRandom();
+    public String generate() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < DIGIT_LENGTH; i++) {
             if (i > 0 && i % BLOCK_SIZE == 0) {
